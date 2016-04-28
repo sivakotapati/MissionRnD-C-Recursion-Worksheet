6dd5a2f73868078210aa5dbec40009eface0e609
sivakotapati <sivakotapati@missionrnd.com>
2016-04-29 00:27:45 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -34,9 +34,58 @@ more parameters .
 */
 
 #include<stdlib.h>
-
+int find_path(int *, int, int, int, int, int, int);
+int is_safe(int*, int, int, int, int);
+int wasHere[10][10] = { 0 };
+int present = 0;
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
+	if (maze == NULL || rows <= 0 || columns <= 0 || x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)
+		return 0;
+	
+	if (find_path(maze, rows, columns, x1, y1, x2, y2) && present){
+		present = 0;
+		return 1;
+	}
+	return 0;
+}
+int find_path(int *maze, int rows, int columns, int x1, int y1, int x2, int y2){
+	int safe = is_safe(maze, rows, columns, x1, y1);
+	if (safe && (x1 == x2&&y1 == y2)){
+		present = 1;
+		return 1;
+	}
+	else if (!safe && (x1 == x2&&y1 == y2))
+	{
+		present = 0;
+		return 1;
+	}
+	else if (!safe){
+		return 0;
+	}
+
+	//move up
+	if (find_path(maze, rows, columns, x1 - 1, y1, x2, y2) == 1) return 1;
+
+	//move forward
+	if (find_path(maze, rows, columns, x1, y1 + 1, x2, y2) == 1) return 1;
+
+	//move downward
+	if (find_path(maze, rows, columns, x1 + 1, y1, x2, y2) == 1) return 1;
+
+	//move backward
+	if (find_path(maze, rows, columns, x1, y1 - 1, x2, y2) == 1) return 1;
+
+	return 0;
+}
+int is_safe(int *maze, int rows, int columns, int x, int y){
+	//if x,y are outside maze
+	if (x < 0 || x >= rows || y < 0 || y >= columns)
+		return 0;
+	//if x1,y1 is not open or visited
+	if ((*(maze + columns * x + y)) == 0 || wasHere[x][y] == 1)
+		return 0;
+	wasHere[x][y] = 1;
 	return 1;
 }
